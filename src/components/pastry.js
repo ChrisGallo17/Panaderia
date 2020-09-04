0b8f4bc1c6cd7523e01dcf6783d49ba0871b51e9
Chris Gallo <cgallo@csustan.edu>
2020-09-03 23:26:22 -0700

    trying to add one image for the pastry components

--- src/components/pastry.js
+++ src/components/pastry.js
@@ -5,6 +5,7 @@ function Pastry(props) {
     return (
         <div className="pastry">
             <h3 className='name'> {props.name} </h3>
+            <img src={"require('./cannoli.jpg')"}></img>
             <p className="desc"> {props.description} </p>
             <p className="cal"> {props.calories} calories</p>
         </div>
