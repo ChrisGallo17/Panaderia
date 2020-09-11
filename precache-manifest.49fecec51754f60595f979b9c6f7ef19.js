035c318913abc048c0bc146988bb36a761bde57b
Chris Gallo <cgallo@csustan.edu>
2020-09-10 20:21:42 -0700

    Updates

---
+++ precache-manifest.49fecec51754f60595f979b9c6f7ef19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0187223af014a21069e9d2da255e2ad",
+    "url": "/Panaderia/index.html"
+  },
+  {
+    "revision": "673460bde5d34b3360d0",
+    "url": "/Panaderia/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ef2263d495f72e4141f4",
+    "url": "/Panaderia/static/css/main.1e875ebd.chunk.css"
+  },
+  {
+    "revision": "673460bde5d34b3360d0",
+    "url": "/Panaderia/static/js/2.976f24da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Panaderia/static/js/2.976f24da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2263d495f72e4141f4",
+    "url": "/Panaderia/static/js/main.4b503c91.chunk.js"
+  },
+  {
+    "revision": "db3302eaf6887fef1100",
+    "url": "/Panaderia/static/js/runtime-main.9b6c6d34.js"
+  },
+  {
+    "revision": "4d78386accb876c79940eae130256d81",
+    "url": "/Panaderia/static/media/logo.4d78386a.png"
+  }
+]);
